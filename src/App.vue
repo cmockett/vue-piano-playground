bf03fcbfdf91cd6dffb47678bdac594e4d7801db
Christian Mockett <cmockett@yahoo.com>
2020-03-07 17:48:00 -0700

    removed helloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <router-link to="/">Home</router-link>
+        <router-link to="piano-home">Piano</router-link>
+        <router-link to="about">About</router-link>
+        <router-view />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        /*margin-top: 60px;*/
+    }
+    .margin-top-100{margin-top:100px;}
+    a{
+        display:inline-block;
+        padding:0 10px;
+        text-decoration: none;
+        color:blue;
+    }
 </style>
